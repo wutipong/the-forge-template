662206e7df722b48c0daa17bbbe45ec612587045
Wutipong Wongsakuldej <wutipong@hotplay.games>
2021-05-09 23:44:42 +0700

    add resource files

--- project/Scripts/postbuild.bat
+++ project/Scripts/postbuild.bat
@@ -4,10 +4,16 @@ SET ProjectDir=%1%
 SET OutputDir=%2%
 
 ECHO Copying shader files.
-XCOPY %ProjectDir%Shaders\ %OutputDir%Shaders\ /L /Y /S
+XCOPY %ProjectDir%Shaders\ %OutputDir%Shaders\ /Y /S
 
 ECHO Copying texture files.
-XCOPY %ProjectDir%Textures\ %OutputDir%Textures\ /L /Y /S
+XCOPY %ProjectDir%Textures\ %OutputDir%Textures\ /Y /S
 
-ECHO Copying fonts files.
-XCOPY %ProjectDir%Fonts\ %OutputDir%Fonts\ /L /Y /S
+ECHO Copying font files.
+XCOPY %ProjectDir%Fonts\ %OutputDir%Fonts\ /Y /S
+
+ECHO Copying dll files.
+XCOPY %ProjectDir%DLLs\ %OutputDir% /Y /S
+
+ECHO Copying GPU config file
+XCOPY %ProjectDir%GPUCfg\ %OutputDir%GPUCfg\ /Y /S
