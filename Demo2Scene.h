7b9796424e6dba62351378e031d4ce4ad477ff27
Wutipong Wongsakuldej <mr_tawan@hotmail.com>
2023-03-12 19:33:14 +0700

    miscellaneous code fixes.

--- Demo2Scene.h
+++ Demo2Scene.h
@@ -12,53 +12,53 @@
 class Demo2Scene : public IScene
 {
 public:
-    virtual void Init(uint32_t imageCount);
-    virtual void Exit();
-    virtual void Load(ReloadDesc *pReloadDesc, Renderer *pRenderer, RenderTarget *pRenderTarget,
-                      RenderTarget *pDepthBuffer, uint32_t imageCount);
-    virtual void Unload(ReloadDesc *pReloadDesc, Renderer *pRenderer);
-    virtual void Update(float deltaTime, uint32_t width, uint32_t height);
-    virtual void PreDraw(uint32_t frameIndex);
-    virtual void Draw(Cmd *pCmd, RenderTarget *pRenderTarget, RenderTarget *pDepthBuffer, uint32_t frameIndex);
+    void Init(uint32_t imageCount) override;
+    void Exit() override;
+    void Load(ReloadDesc *pReloadDesc, Renderer *pRenderer, RenderTarget *pRenderTarget, RenderTarget *pDepthBuffer,
+              uint32_t imageCount) override;
+    void Unload(ReloadDesc *pReloadDesc, Renderer *pRenderer) override;
+    void Update(float deltaTime, uint32_t width, uint32_t height) override;
+    void PreDraw(uint32_t frameIndex) override;
+    void Draw(Cmd *pCmd, RenderTarget *pRenderTarget, RenderTarget *pDepthBuffer, uint32_t frameIndex) override;
 
     bool OnInputAction(InputActionContext *ctx);
 
 private:
     static constexpr size_t OBJECT_COUNT = 3;
 
-    int cubeVertexCount;
-    Buffer *pCubeVertexBuffer;
+    int cubeVertexCount{};
+    Buffer *pCubeVertexBuffer{};
 
-    int sphereVertexCount;
-    Buffer *pSphereVertexBuffer;
+    int sphereVertexCount{};
+    Buffer *pSphereVertexBuffer{};
 
-    int boneVertexCount;
-    Buffer *pBoneVertexBuffer;
+    int boneVertexCount{};
+    Buffer *pBoneVertexBuffer{};
 
-    Buffer **pObjectUniformBuffers;
-    Buffer **pSceneUniformBuffer;
+    Buffer **pObjectUniformBuffers{};
+    Buffer **pSceneUniformBuffer{};
 
-    ICameraController *pCameraController;
+    ICameraController *pCameraController{};
 
-    Shader *pShader;
-    RootSignature *pRootSignature;
-    DescriptorSet *pDescriptorSetSceneUniform;
-    DescriptorSet *pDescriptorSetObjectUniform;
+    Shader *pShader{};
+    RootSignature *pRootSignature{};
+    DescriptorSet *pDescriptorSetSceneUniform{};
+    DescriptorSet *pDescriptorSetObjectUniform{};
 
-    Pipeline *pPipeline;
+    Pipeline *pPipeline{};
 
     struct ObjectUniformBlock
     {
         mat4 Transform;
         float4 Color;
-    } objects[OBJECT_COUNT];
+    } objects[OBJECT_COUNT]{};
 
     enum class ObjectType
     {
         Cube,
         Sphere,
         Bone,
-    } objectTypes[OBJECT_COUNT];
+    } objectTypes[OBJECT_COUNT]{};
 
     static constexpr size_t DIRECTIONAL_LIGHT_COUNT = 2;
     struct SceneUniformBlock
@@ -66,14 +66,16 @@ private:
         // Camera
         vec4 CameraPosition;
         CameraMatrix ProjectView;
+
         // Directional Light;
         float4 LightDirection[DIRECTIONAL_LIGHT_COUNT];
         float4 LightColor[DIRECTIONAL_LIGHT_COUNT];
         float4 LightAmbient[DIRECTIONAL_LIGHT_COUNT];
         float4 LightIntensity[DIRECTIONAL_LIGHT_COUNT];
-    } scene;
+    } scene{};
 
-    UIComponent *pObjectWindow;
+    UIComponent *pObjectWindow{};
+
     void resetLightSettings();
 };
 
